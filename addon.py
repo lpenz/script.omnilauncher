1790df1c1dbff77cd3e6a5fcc9f9504c4ce71791
Leandro Lisboa Penz <lpenz@lpenz.org>
2016-01-25 22:49:17 -0200

    "command" target works
    Next: cleanup and really implement functionality

--- addon.py
+++ addon.py
@@ -1,7 +1,12 @@
 '''Omnilauncher entry point'''
 
 import sys
+try:
+    from urllib.parse import parse_qs
+except ImportError:
+    from urlparse import parse_qs
 
 if (__name__ == "__main__"):
     import resources.lib.omnilauncher
-    resources.lib.omnilauncher.main(int(sys.argv[1]))
+    resources.lib.omnilauncher.main(
+        sys.argv[0], int(sys.argv[1]), parse_qs(sys.argv[2][1:]))
--- resources/lib/omnilauncher.py
+++ resources/lib/omnilauncher.py
@@ -1,11 +1,19 @@
 '''Core code of omnilauncher'''
 
 import os
+try:
+    import urllib as urlencodemodule
+except ImportError:
+    import urllib.parse as urlencodemodule
+
+import subprocess
 import xbmcgui
 import xbmcplugin
 import xml.etree.ElementTree as xmltree
 
-pjoin = os.path.join
+pj = os.path.join
+
+URI_TOP = u'plugin://script.omnilauncher/'
 
 
 class Omnilauncher(object):
@@ -14,32 +22,52 @@ class Omnilauncher(object):
         self.handle = handle
         self.home = xbmcplugin.getSetting(handle, 'home')
 
-    def run(self):
+    def run(self, uri, args):
+        if len(args) == 0:
+            self.top(uri, args)
+            return
+        if 'command' in args:
+            subprocess.call(args['command'], shell=True)
+
+    def top(self, uri, args):
         for d in os.listdir(self.home):
-            itemfile = pjoin(self.home, d, 'omniitem.nfo')
-            try:
-                et = xmltree.parse(itemfile)
-            except Exception:
-                continue
-            li = xbmcgui.ListItem(et.find('./title').text)
-            nfo = {}
-            for etinfo in et.iter('info'):
-                for etfield in etinfo.iter():
-                    if etfield == etinfo:
-                        continue
-                    nfo[etfield.tag] = etfield.text
-            li.setInfo('video', nfo)
-            art = {}
-            for etart in et.iter('art'):
-                for etfield in etart.iter():
-                    if etfield == etart:
-                        continue
-                    art[etfield.tag] = pjoin(self.home, d, etfield.text)
-            li.setArt(art)
-            xbmcplugin.addDirectoryItem(self.handle, u'', li)
+            self.item_add(pj(self.home, d, 'omniitem.nfo'))
         xbmcplugin.endOfDirectory(self.handle)
 
+    def item_add(self, itemfile):
+        try:
+            et = xmltree.parse(itemfile)
+        except Exception:
+            return
+        li = xbmcgui.ListItem(et.find('./title').text)
+        nfo = {}
+        for etinfo in et.iter('info'):
+            for etfield in etinfo.iter():
+                if etfield == etinfo:
+                    continue
+                nfo[etfield.tag] = etfield.text
+        li.setInfo('video', nfo)
+        art = {}
+        for etart in et.iter('art'):
+            for etfield in etart.iter():
+                if etfield == etart:
+                    continue
+                art[etfield.tag] = pj(
+                    os.path.dirname(itemfile),
+                    etfield.text)
+        li.setArt(art)
+        for target in et.iter('target'):
+            if target.get('type') == 'command':
+                uri = URI_TOP + '?' + \
+                    urlencodemodule.urlencode(
+                        {
+                            'itemfile': itemfile,
+                            'command': target.text
+                        })
+                xbmcplugin.addDirectoryItem(
+                    self.handle, uri, li, isFolder=False)
 
-def main(handle):
+
+def main(uri, handle, args):
     o = Omnilauncher(handle)
-    o.run()
+    o.run(uri, args)
